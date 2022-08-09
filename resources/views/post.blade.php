375d4091be331eb0f822f4aa8b7f1a8243e3ae91
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-09 14:02:05 +0530

    refactor find method and css fix

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,18 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="/app.css">
     <title>Document</title>
 </head>
 <body>
     <article>
-        {{-- <h1>First Post</h1>
-        <p>
-            First PostFirst PostFirst PostFirst Post
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat est quaerat porro, voluptas facilis iure unde nesciunt esse minus vel perspiciatis? Consequatur illo molestias beatae! Minima dolorem architecto delectus obcaecati!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat est quaerat porro, voluptas facilis iure unde nesciunt esse minus vel perspiciatis? Consequatur illo molestias beatae! Minima dolorem architecto delectus obcaecati!
-        </p> --}}
-        <?=$post?>
+        <h1><?=$post->title?></h1>
+        <p><?=$post->body?></p>
     </article>
     <a href="/">Go Back</a>
 
