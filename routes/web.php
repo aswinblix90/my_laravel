ba55f858e61847bf0ee24f9b768f0ed284aa6635
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-09 12:56:37 +0530

    remove commented code

--- routes/web.php
+++ routes/web.php
@@ -18,26 +18,9 @@ use Illuminate\Support\Facades\File;
 */
 
 Route::get('/', function () {
-    // ddd($posts[0]->getContents());
-    // $files = File::files(resource_path('posts/'));
-
-    // $test = YamlFrontMatter::parseFile($files[0]);
-    // dd($posts);
     $posts = [];
     $posts = collect(File::files(resource_path("posts/")))
-                // ->map(function($file){
-                //     return YamlFrontMatter::parseFile($file);
-                // }
                 ->map(fn($file)=> YamlFrontMatter::parseFile($file))
-                // ->map(function($document){
-                //     return new Post(
-                //         $document->title,
-                //         $document->date,
-                //         $document->body(),
-                //         $document->excerpt,
-                //         $document->slug
-                //     );
-                // });
                 ->map(fn($document)=> new Post(
                     $document->title,
                     $document->date,
@@ -45,27 +28,12 @@ Route::get('/', function () {
                     $document->excerpt,
                     $document->slug
                 ));
-
-    // $posts = array_map(function($file){
-    //     $document = YamlFrontMatter::parseFile($file);
-    //     return new Post(
-    //         $document -> title,
-    //         $document -> date,
-    //         $document -> body(),
-    //         $document -> excerpt,
-    //         $document -> slug
-    //     );
-    // },$files);
-    // dd($posts);
     return view('posts',[
         'posts' => $posts
     ]);
 });
 
 Route::get('posts/{post}', function($slug){
-    // dd($path);
-
-    // $post = file_get_contents($path);
     return view('/post',[
         'post' => Post::find($slug)
     ]);
