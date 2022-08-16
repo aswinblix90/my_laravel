eeaf9a0824b6993fe5e1a77acb80074f30ac4e78
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-16 15:52:09 +0530

    seed db with faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,55 +17,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
+        Post::factory(5)->create();
+        // User::truncate();
+        // Post::truncate();
+        // Category::truncate();
         
-        $user = User::factory()->create();
-        $user1 = User::factory()->create();
+        // $user = User::factory()->create();
+        // $user1 = User::factory()->create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-        Post::create([
-            'category_id' => $family -> id,
-            'user_id' => $user -> id,
-            'title' => 'My family post',
-            'slug' => 'my-family-post' ,
-            'excerpt' => 'My family post Lorem ipsum dolor sit',
-            'body' => 'My family post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
-                        Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
-        ]);
-        Post::create([
-            'category_id' => $work->id,
-            'user_id' => $user1 -> id,
-            'title' => 'My work post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'My work post Lorem ipsum dolor sit amet co',
-            'body' => 'My work post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
-                        Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
-        ]);
-        Post::create([
-            'category_id' => $personal->id,
-            'user_id' => $user -> id,
-            'title' => 'My personal post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'My personal post Lorem ipsum dolor sit amet consectetu',
-            'body' => 'My personal post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
-                        Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
-        ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
         // ]);
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+        // Post::create([
+        //     'category_id' => $family -> id,
+        //     'user_id' => $user -> id,
+        //     'title' => 'My family post',
+        //     'slug' => 'my-family-post' ,
+        //     'excerpt' => 'My family post Lorem ipsum dolor sit',
+        //     'body' => 'My family post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
+        //                 Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
+        // ]);
+        // Post::create([
+        //     'category_id' => $work->id,
+        //     'user_id' => $user1 -> id,
+        //     'title' => 'My work post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => 'My work post Lorem ipsum dolor sit amet co',
+        //     'body' => 'My work post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
+        //                 Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
+        // ]);
+        // Post::create([
+        //     'category_id' => $personal->id,
+        //     'user_id' => $user -> id,
+        //     'title' => 'My personal post',
+        //     'slug' => 'my-personal-post',
+        //     'excerpt' => 'My personal post Lorem ipsum dolor sit amet consectetu',
+        //     'body' => 'My personal post Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo distinctio natus consectetur quam iusto harum numquam nesciunt, dolorum, sint odit saepe in. Perspiciatis quos veritatis illum voluptates, quam mollitia beatae
+        //                 Voluptatum itaque error facere, atque fugiat libero eos exercitationem non repellendus aperiam aspernatur veniam illo, officiis quas mollitia assumenda voluptate ipsa accusantium quis quia consequuntur. Sint dignissimos ab ea nemo'
+        // ]);
+
     }
 }
