b160fe000bbdec88782313c64b084e11f156e2bf
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-18 12:19:25 +0530

    change blogs layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,28 @@
 
 <x-layout>
-    @foreach ($posts as $post)
+    {{-- @foreach ($posts as $post) --}}
     {{-- @dd($loop) --}}
-        <article>
+        {{-- <article>
             <a href="posts/{{$post->slug}}"><h1>{!!$post->title!!}</h1></a>
             <p>By <a href="authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="categories/{{$post->category->id}}">{{$post->category->name}}</a></p>
-
             <div>{{$post->excerpt}}</div>
-        </article>
-    @endforeach
+        </article> --}}
+    {{-- @endforeach --}}
+        @include('_posts-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+                <x-post-featured-card></x-post-featured-card>
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card></x-post-card>
+                <x-post-card></x-post-card>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card></x-post-card>
+                <x-post-card></x-post-card>
+                <x-post-card></x-post-card>
+            </div>
+        </main>
 </x-layout>
 
