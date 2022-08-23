40c10a6dbf93fdcb03ee2cd4e9cf1147cee9238f
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-23 12:20:47 +0530

    change  dropdown as components

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,9 @@
+@props(['trigger'])
+<div x-data="{show: false}" @click.away="show=false">
+    <div @click="show= !show">
+        {{$trigger}}
+    </div>
+    <div class="py-2 absolute bg-gray-100 w-full shadow rounded-xl mt-2 z-50" x-show="show" style="display: none">
+        {{$slot}}
+    </div>
+</div>
