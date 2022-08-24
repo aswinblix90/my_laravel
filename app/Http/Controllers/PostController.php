49c5ccd2af3be7ebea8ce48eac1d8d9e264548f7
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-24 15:42:50 +0530

    add get posts by seperate method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,9 @@ class PostController extends Controller
 {
     //
     public function index(){
-        $posts = Post::latest();
-        if (request('search')) {
-            $posts
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+
         return view('posts', [
-            'posts' => $posts->get(),
+            'posts' => $this->getPosts(),
             'categories' => Category::all()
         ]);
     }
@@ -25,5 +20,14 @@ class PostController extends Controller
         return view('/post', [
             'post' => $post
         ]);
+    }
+    public function getPosts(){
+        $posts = Post::latest();
+        if (request('search')) {
+            $posts
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+        return $posts->get();
     }
 }
