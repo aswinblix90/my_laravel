857d12939670d86e841a6f8540bd39311a0e0ac6
aswinblix <aswin.blix@bitsnpixs.com>
2022-08-24 16:16:13 +0530

    add scoped filter

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index(){
 
         return view('posts', [
-            'posts' => $this->getPosts(),
+            'posts' => Post::latest()->filter(request(['search']))->get(),
             'categories' => Category::all()
         ]);
     }
@@ -20,14 +20,5 @@ class PostController extends Controller
         return view('/post', [
             'post' => $post
         ]);
-    }
-    public function getPosts(){
-        $posts = Post::latest();
-        if (request('search')) {
-            $posts
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
-        return $posts->get();
     }
 }
